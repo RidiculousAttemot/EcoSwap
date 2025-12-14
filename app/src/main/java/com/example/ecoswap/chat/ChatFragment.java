2f23dcee12328fbc8ea1c3d1ecfd623127051ff1
Arwin E. Dante <arwindante2@gmail.com>
2025-12-14 18:25:44 +0800

    feat: Add Instant class import for improved date handling in ChatFragment

--- app/src/main/java/com/example/ecoswap/chat/ChatFragment.java
+++ app/src/main/java/com/example/ecoswap/chat/ChatFragment.java
@@ -33,6 +33,7 @@ import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.card.MaterialCardView;
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
 import com.google.gson.JsonObject;
+import java.time.Instant;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
